98647be58951e9fe228335dec9ed25ce6a7a6ded
Anh Tuong Nguyen <anhtuongmr@gmail.com>
2017-05-08 01:18:05 +0700

    Initial Commit

---
+++ UIScrollViewKyBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIScrollViewKyBoard
+//
+//  Created by Anh Tuong Nguyen on 5/8/17.
+//  Copyright © 2017 Anh Tuong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
